03a5fb1118d79b3a22b6302ebdee86092c1ecf27
Manan <mananvaghasiya15@gmail.com>
2026-07-03 14:30:41 +0530

    Update handle_game.py

--- handle_game.py
+++ handle_game.py
@@ -4,7 +4,19 @@ import sys
 lane1 = lane()
 gid = 1
 tid = 1
+
+def display_command():
+    print("SPAWN <lane> <enemyType>")
+    print("ADD_TOWER <lane> <position>")
+    print(" RUN_TURN")
+    print("")
+    print("STATUS")
+    print("HISTORY")
+    print("EXIT")
+
+    
 while(True):
+    display_command()
     command = input()
     if(command == "SPAWN lane1 goblin"):
         id = 'E' + str(gid)
@@ -18,6 +30,18 @@ while(True):
             id = 'T' + str(tid)
             lane1.create_tower(id,position)
             tid += 1
+        else:
+            print("Enter valid input")
+
+    elif(command.startswith("ADD_SNIPER_TOWER lane1")):
+        parts = command.split()
+        position = int(parts[2])
+        if(position >= 0 and position <= 4):
+            id = 'ST' + str(tid)
+            lane1.create_sniper_tower(id,position)
+            tid += 1
+        else:
+            print("Enter valid input")
 
     elif(command == "RUN_TURN"):
         lane1.run_turn()
