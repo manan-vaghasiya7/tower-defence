344208075d1f6d42f24a7fae059981932a79b4fa
Manan <mananvaghasiya15@gmail.com>
2026-07-06 14:35:03 +0530

    slow tower added and also the function to create it is added

---
+++ defenses_and_troops/slow_tower.py
@@ -0,0 +1,6 @@
+class SlowTower:
+    def __init__(self,id,position):
+        self.active = True
+        self.id = id
+        self.position = position
+        
--- lane.py
+++ lane.py
@@ -1,6 +1,7 @@
-from goblin import Goblin
-from tower import Tower
-from sniper_tower import SniperTower
+from defenses_and_troops.goblin import Goblin
+from defenses_and_troops.tower import Tower
+from defenses_and_troops.sniper_tower import SniperTower
+from defenses_and_troops.slow_tower import SlowTower
 from run_components.run_towers import run_towers
 from run_components.run_goblins import run_goblins
 from run_components.run_sniper_tower import run_sniper_towers
@@ -15,6 +16,7 @@ class lane:
         self.total_components = [[],[],[],[],[],[]]
         self.base = 10
         self.turn = 0
+        self.slow_towers = {}
 
 
     def create_goblin(self,id):
@@ -39,6 +41,11 @@ class lane:
         self.historyi.append(f"Added {self.sniper_towers[id].id} sniper tower on lane1 of range {self.sniper_towers[id].range} at position {self.sniper_towers[id].position}.")
         print(f"Added {self.sniper_towers[id].id} sniper tower on lane1 of range {self.sniper_towers[id].range} at position {self.sniper_towers[id].position}.")
 
+    def create_slow_tower(self,id,position):
+        self.slow_towers[id] = SlowTower(id,position)
+        self.total_components[position].append(id)
+        self.historyi.append(f"Added {self.slow_towers[id].id} slow tower on lane1 at position {self.slow_towers[id].position}.")
+        print(f"Added {self.slow_towers[id].id} slow tower on lane1 {self.slow_towers[id].position}.")
 
 
     def run_turn(self):
