df57feef69c56c0ca7ff8bddfab667692a3ab89c
Manan <mananvaghasiya15@gmail.com>
2026-07-06 18:38:18 +0530

    Create runner.py

---
+++ defenses_and_troops/runner.py
@@ -0,0 +1,9 @@
+class runner:
+    def __init__(self,id):
+        self.id = id
+        self.health = 2
+        self.speed = 2
+        self.position = 0
+
+    def run_runner(self):
+        pass
