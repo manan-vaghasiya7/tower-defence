ab07a17ca4554deb4743222d4b0ff1325236acfa
Manan <mananvaghasiya15@gmail.com>
2026-07-06 15:32:15 +0530

    now the towers will attack the troops which is closest to them

--- run_components/run_towers.py
+++ run_components/run_towers.py
@@ -9,17 +9,17 @@ def run_towers(towers,goblins,historyi,total_components,base,turn):
                 if abs(tower_position - goblins[goblin].position) < abs(tower_position - goblins[nearest_to_tower].position):
                     nearest_to_tower = goblin
 
-            if abs(goblins[nearest_to_tower].position - towers[tower].position) >= 1:
-                if goblins[goblin].health > 1:
-                    goblins[goblin].health -= 1
+            if abs(goblins[nearest_to_tower].position - towers[tower].position) <= 1:
+                if goblins[nearest_to_tower].health > 1:
+                    goblins[nearest_to_tower].health -= 1
                     towers[tower].active = False
-                    historyi.append(f"Turn : {turn} , {towers[tower].id} attacked {goblins[goblin].id} for 1 damage. {goblins[goblin].id} hp={goblins[goblin].health}. ")
-                    print(f"{towers[tower].id} attacked {goblins[goblin].id} for 1 damage. {goblins[goblin].id} hp={goblins[goblin].health}. ")
+                    historyi.append(f"Turn : {turn} , {towers[tower].id} attacked {goblins[nearest_to_tower].id} for 1 damage. {goblins[nearest_to_tower].id} hp={goblins[nearest_to_tower].health}. ")
+                    print(f"{towers[tower].id} attacked {goblins[nearest_to_tower].id} for 1 damage. {goblins[nearest_to_tower].id} hp={goblins[nearest_to_tower].health}. ")
 
                 else:
-                    historyi.append(f"Turn : {turn} , {goblins[goblin].id} is killed ")
-                    print(f"{goblins[goblin].id} is killed ")
-                    total_components[goblins[goblin].position].remove(goblins[goblin].id)
+                    historyi.append(f"Turn : {turn} , {goblins[nearest_to_tower].id} is killed ")
+                    print(f"{goblins[nearest_to_tower].id} is killed ")
+                    total_components[goblins[nearest_to_tower].position].remove(goblins[nearest_to_tower].id)
                     towers[tower].active = False
                     goblins.pop(nearest_to_tower)
 
