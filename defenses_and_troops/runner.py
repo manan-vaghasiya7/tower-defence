acf8e37b5f600bb233bf5f4927fe31093ab57b8b
Manan <mananvaghasiya15@gmail.com>
2026-07-07 12:39:13 +0530

    logic added in troops to lower their speed by 1 for that turn

--- defenses_and_troops/runner.py
+++ defenses_and_troops/runner.py
@@ -6,23 +6,49 @@ class Runner:
         self.health = 2
         self.speed = 2
         self.position = 0
+        self.slow_applied = False
+        self.dont_move = False
 
     def run_runner(self,historyi,total_components,base,turn,to_pop):
-        if self.position >= 3:
-            base -= 1
-            historyi.append(f"Turn : {turn} , {self.id} attacked Base for 1 damage and {self.id} is removed. Base hp ={base} ")
-            print(f"{self.id} attacked Base for 1 damage and {self.id} is removed. Base hp ={base}  ")
-            total_components[self.position].remove(self.id)
-            to_pop.append(self.id)
-            if base == 0:
-                historyi.append("YOU WON")
-                print("YOU WON")
-                sys.exit()
+        if self.dont_move:
+            self.dont_move = False
+            if self.position >= 4:
+                base -= 1
+                historyi.append(f"Turn : {turn} , {self.id} attacked Base for 1 damage and {self.id} is removed. Base hp ={base} ")
+                print(f"{self.id} attacked Base for 1 damage and {self.id} is removed. Base hp ={base}  ")
+                total_components[self.position].remove(self.id)
+                to_pop.append(self.id)
+                if base == 0:
+                    historyi.append("YOU WON")
+                    print("YOU WON")
+                    sys.exit()
 
-        else: 
-            self.position += 2
-            historyi.append(f"{self.id} moved from {self.position-2} to {self.position} ")
-            print(f"{self.id} moved from {self.position-2} to {self.position} ")
-            total_components[self.position-2].remove(self.id)
-            total_components[self.position].append(self.id)
+            else: 
+                self.position += 1
+                historyi.append(f"{self.id} moved from {self.position-1} to {self.position} ")
+                print(f"{self.id} moved from {self.position-1} to {self.position} ")
+                total_components[self.position-1].remove(self.id)
+                total_components[self.position].append(self.id)
+        else:
+            if self.position >= 3:
+                base -= 1
+                historyi.append(f"Turn : {turn} , {self.id} attacked Base for 1 damage and {self.id} is removed. Base hp ={base} ")
+                print(f"{self.id} attacked Base for 1 damage and {self.id} is removed. Base hp ={base}  ")
+                total_components[self.position].remove(self.id)
+                to_pop.append(self.id)
+                if base == 0:
+                    historyi.append("YOU WON")
+                    print("YOU WON")
+                    sys.exit()
+
+            else: 
+                self.position += 2
+                historyi.append(f"{self.id} moved from {self.position-2} to {self.position} ")
+                print(f"{self.id} moved from {self.position-2} to {self.position} ")
+                total_components[self.position-2].remove(self.id)
+                total_components[self.position].append(self.id)
         return base
+    
+    def make_slow(self):
+        self.slow_applied = True
+        self.dont_move = True
